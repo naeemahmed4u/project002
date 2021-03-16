eb2c766193878eeaf9ef4060627fc0d7c24b7b0e
naeemahmed4u <76744352+naeemahmed4u@users.noreply.github.com>
2021-03-16 22:55:54 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React (Naeem Ahmed)
+          Learn React (Naeem Ahmed can deploy auto)
         </a>
       </header>
     </div>
